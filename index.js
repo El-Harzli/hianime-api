4783363e96153312ea3fedc17ad5557df5f537a7
Ayman El Harzli <aymanmoschino@gmail.com>
2025-11-11 18:13:13 +0100

    feat(user): add User model and basic scaffolding for controllers and routes

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './src/config/db.js';
 
+import authRoute from './src/routes/auth.route.js';
+
 dotenv.config();
 const PORT = process.env.PORT || '5000';
 const app = express();
+
+app.use('/api/v2/auth', authRoute);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
